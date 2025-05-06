7dbeee79e4c90a2985c5d90cdbf95615a95a6187
SUGESH R <101317215+SUGESHRAMALINGAM@users.noreply.github.com>
2025-05-06 18:15:25 +0530

    Update Test_api.py

--- Ml project/Test_api.py
+++ Ml project/Test_api.py
@@ -1,23 +1,27 @@
+import sys
+import os
+
+# Add the root directory (where app.py is) to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app  # imports app instance from app.py
+
 from fastapi.testclient import TestClient
-from app import app
 
 client = TestClient(app)
 
 def test_predict_endpoint():
-    sample_data = {
+    response = client.post("/predict", json={
         "Warehouse_block": "F",
         "Mode_of_Shipment": "Flight",
         "Customer_care_calls": 3,
         "Customer_rating": 4,
-        "Cost_of_the_Product": 100,
+        "Cost_of_the_Product": 250,
         "Prior_purchases": 2,
-        "Product_importance": "low",
-        "Gender": "M",
-        "Discount_offered": 10,
-        "Weight_in_gms": 500
-    }
-
-    response = client.post("/predict", json=sample_data)
+        "Product_importance": "high",
+        "Gender": "F",
+        "Discount_offered": 20,
+        "Weight_in_gms": 2000
+    })
     assert response.status_code == 200
     assert "prediction" in response.json()
-    assert response.json()["prediction"] in [0, 1]
